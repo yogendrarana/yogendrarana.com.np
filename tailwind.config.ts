7d9e0ba2757518f3c486760557cf801111163c16
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-10-15 21:33:58 +0545

    fix: fixed tailwind config error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config = {
     prefix: "",
     theme: {
     	container: {
-    		center: 'true',
+    		center: true,
     		padding: {
     			DEFAULT: '1rem',
     			lg: '5rem',
@@ -47,29 +47,11 @@ const config = {
     				to: {
     					height: '0'
     				}
-    			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
     			}
     		},
     		animation: {
     			'accordion-down': 'accordion-down 0.2s ease-out',
     			'accordion-up': 'accordion-up 0.2s ease-out',
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
     		}
     	}
     },
